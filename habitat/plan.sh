60dc1675b797d2f5d64b1379b80d8839257c4e26
Bryan Williams <bryanwilliams@a-v33exhtd8.carfax.cfx>
2018-11-05 13:17:46 -0600

    Use my ImageMagick package

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=habitatize
 pkg_origin=chef_training
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.1.0"
-pkg_deps=( core/imagemagick )
+pkg_version="0.2.0"
+pkg_deps=( chef_training/imagemagick )
 
